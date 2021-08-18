602e6ca693ba2ec4390dd7a6956b3beee59650a1
yasin miah <eiasin440@gmail.com>
2021-08-18 18:26:43 +0600

    product-calculation-done

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('I am in a separate file')
+function updateItems(product, price, isIncreasing) {
+    const productsNumber = document.getElementById(product + '-number');
+    let productsNumberValue = productsNumber.value;
+    if (isIncreasing == true) {
+        productsNumberValue = parseInt(productsNumberValue) + 1;
+    }
+    else if (productsNumberValue > 0) {
+        productsNumberValue = parseInt(productsNumberValue) - 1;
+    }
+    productsNumber.value = productsNumberValue;
+    //update case-price
+    const productsPrice = document.getElementById(product + '-price');
+    productsPrice.innerText = productsNumberValue * price;
+}
+//phone increase and dicrease
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateItems('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateItems('phone', 1219, false);
+});
+//case increase and dicrease
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateItems('case', 59, true);
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateItems('case', 59, false);
+});
